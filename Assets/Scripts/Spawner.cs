41b863504086d1f16f5895a09a3607fc79e0955b
Daniel-W <danijw01@gmail.com>
2022-04-12 20:44:00 +0200

    Some extra changes

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Random : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
-    public float speed;
     // Start is called before the first frame update
     void Start()
     {
