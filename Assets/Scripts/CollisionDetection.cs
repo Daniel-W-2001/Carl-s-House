0e6b9c11c94930384be2a6c738768fc5b2c360cb
Daniel-W <danijw01@gmail.com>
2022-04-26 20:39:16 +0200

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -8,15 +8,16 @@ public class CollisionDetection : MonoBehaviour
 
     public PlayerAttack bp;
     public float knockback;
-    public float knockbackUp;
+    //public float knockbackUp;
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Enemy" && bp.IsAttacking)
         {
          
             Debug.Log("Has hit the enemy");
+            other.gameObject.GetComponent<Rigidbody>().AddRelativeForce(Vector3.back * knockback, ForceMode.Impulse);
+
             other.GetComponent<EnemyManager>().HurtEnemy(damageToGive);
-            other.gameObject.GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * knockback, ForceMode.Impulse);
             //other.gameObject.GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * knockbackUp, ForceMode.Impulse);
 
             //AddRelativeForce(Vector3.back * knockback, ForceMode.Impulse);
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Animations;
 public class Spawner : MonoBehaviour
 {
     public int DamageWhileAirborne;
-    public bool animIsPlaying;
+    public bool AnimIsPlaying;
+
+    public Animator vaseAnim;
 
     public Transform Spawnpoint;
     public float timeBetweenSpawn;
@@ -14,24 +17,40 @@ public class Spawner : MonoBehaviour
     public GameObject[] EnemyArray;
     void Start()
     {
-        animIsPlaying = false;
+
+        StartCoroutine(RootApply());
+        
+        vaseAnim.GetComponent<Animator>();
     }
     void FixedUpdate()
     {
         if (Time.time > spawnTime)
         {
+
             spawnTime = Time.time + timeBetweenSpawn;
             Spawn();
         }
     }
     void Spawn()
     {
+
         Vector3 start = Spawnpoint.position;
 
         int Enemyspawn = Random.Range(0, EnemyArray.Length);
 
         GameObject obj = (GameObject)Instantiate(EnemyArray[Enemyspawn], start, transform.rotation);
 
-           
+        obj.GetComponent<NavMeshAgent>().enabled = true;
+        obj.GetComponent<Animator>().SetBool("AnimIsPlaying", true);
     }
-}
+    IEnumerator RootApply()
+    {
+        yield return new WaitForSeconds(4f);
+
+        vaseAnim.applyRootMotion = true;
+    }
+        
+
+
+    }
+
