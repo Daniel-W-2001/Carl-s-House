909a4b7524c5301db0256620b4373d02fe5957cb
Daniel-W <danijw01@gmail.com>
2022-04-28 20:50:40 +0200

    Player moves faster on water

--- Assets/CollideTest.cs
+++ Assets/CollideTest.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class CollideTest : MonoBehaviour
 {
+    public FirstPersonController controller;
+    public bool IsColliding;
+
+    void Start()
+    {
+        IsColliding = false;
+    }
+    void FixedUpdate()
+    {
+        if (IsColliding == false)
+        {
+            controller.GetComponent<FirstPersonController>().walkSpeed = 7f;
+            controller.GetComponent<FirstPersonController>().sprintSpeed = 14f;
+        }
+    }
     private void OnParticleCollision(GameObject other)
     {
-        if(other.gameObject.name == "FirstPersonController")
+        if (other.gameObject.name == "FirstPersonController" && IsColliding == false)
         {
-            Debug.Log("Collide");
+            IsColliding = true;
+            other.gameObject.GetComponent<FirstPersonController>().walkSpeed = 14f;
+            other.gameObject.GetComponent<FirstPersonController>().sprintSpeed = 21f;
+        } else
+        {
+            IsColliding = false;
         }
-        
     }
 }
+
