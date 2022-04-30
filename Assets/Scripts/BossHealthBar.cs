f8b5b7e6d18424ec752e7bbf03f6d7e972a93dc6
Daniel-W <danijw01@gmail.com>
2022-04-30 22:35:29 +0200

    particle show

--- Assets/Scripts/BossHealthBar.cs
+++ Assets/Scripts/BossHealthBar.cs
@@ -8,12 +8,15 @@ public class BossHealthBar : MonoBehaviour
     public GameObject House;
     public GameObject Spawning;
     public Animator HouseAnim;
-
+    public GameObject Particle_1, Particle_2, Particle_3;
     public int BossCurrentHealth;
     public int MaxHealth;
 
     void Start()
     {
+        Particle_1.SetActive(false);
+        Particle_2.SetActive(false);
+        Particle_3.SetActive(false);
         BossCurrentHealth = MaxHealth;
     }
     private void OnCollisionEnter(Collision collision)
@@ -31,6 +34,9 @@ public class BossHealthBar : MonoBehaviour
             HouseAnim.SetBool("HouseDies", true);
             Destroy(Spawning);
             StartCoroutine(DestroyHouse());
+            Particle_1.SetActive(true);
+            Particle_2.SetActive(true);
+            Particle_3.SetActive(true);
         }
     }
     IEnumerator DestroyHouse()
@@ -38,6 +44,9 @@ public class BossHealthBar : MonoBehaviour
         yield return new WaitForSeconds(4.4f);
 
         Destroy(House);
+        Destroy(Particle_1, 3.0f);
+        Destroy(Particle_2, 3.0f);
+        Destroy(Particle_3, 3.0f);
     }
 }
 
